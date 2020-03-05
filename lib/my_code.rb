a06b25ac425a7099e283c169aec6ad76374ccb6f
Josh Beglan <josh_beglan@yahoo.com>
2020-03-05 13:38:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,7 +46,7 @@ def map_to_square(source_array)
   squared_arr
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   i = 0
   while i < source_array.length
     num = source_array[i]
